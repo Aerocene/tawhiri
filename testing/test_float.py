8c527be3d3714ed7b64df9afc36545af8ceccb98
i-n-g-o <ingo.randolf@servus.at>
2020-01-21 18:05:42 +0100

    adapt float example

--- testing/test_float.py
+++ testing/test_float.py
@@ -4,9 +4,11 @@ sys.path.append(join(split(abspath(__file__))[0], '..'))
 
 import time
 import itertools
-from datetime import datetime
+from datetime import datetime, timedelta
 import json
 import calendar
+
+from suntime import Sun, SunTimeException
 
 from tawhiri import solver, models, kml
 from tawhiri.dataset import Dataset as WindDataset
@@ -16,14 +18,47 @@ from tawhiri.warnings import WarningCounts
 lat0 = 52.5563
 lng0 = 360 - 3.1970
 alt0 = 0.0
-t0 = calendar.timegm(datetime(2014, 2, 19, 15).timetuple())
-tE = calendar.timegm(datetime(2014, 2, 20, 6, 1).timetuple())
+offsetDays = 3
+useSunrise = False
+timeResolution = 4000
 
 wind = WindDataset.open_latest()
 warningcounts = WarningCounts()
-stages = models.float_profile(2.0, 200000, tE, wind, warningcounts)
 
-rise, float = solver.solve(t0, lat0, lng0, alt0, stages)
+#
+new_datetime = wind.ds_time
+
+# check sunrise
+if useSunrise == True:
+    new_datetime = Sun(lat0, lng0).get_sunrise_time(wind.ds_time.date())
+
+    if new_datetime.hour < wind.ds_time.hour:
+        # can only start next day
+        new_datetime = Sun(lat0, lng0).get_sunrise_time(wind.ds_time.date() + timedelta(days=1))
+
+
+# add offset days
+new_datetime = new_datetime + timedelta(days=offsetDays)
+
+ds_end = wind.ds_time + timedelta(hours=wind.forecast_hours(), minutes=-1)
+ds_end_ts = time.mktime(ds_end.timetuple())
+
+#t0 = calendar.timegm(datetime(2020, 1, 7, 21).timetuple())
+#tE = calendar.timegm(datetime(2014, 2, 20, 6, 1).timetuple())
+
+t0 = time.mktime(new_datetime.timetuple())
+tE = ds_end_ts
+
+float_alt = 5500
+
+print "wind: ", wind.ds_time
+print "start: ", datetime.fromtimestamp(t0)
+print "end: ", ds_end
+print "time resolution: ", timeResolution
+
+
+stages = models.float_profile(2.0, float_alt, tE, wind, warningcounts)
+rise, float = solver.solve(t0, lat0, lng0, alt0, stages, timeResolution)
 
 assert rise[-1] == float[0]
 
